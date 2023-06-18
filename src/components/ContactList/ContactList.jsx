12a3b0606a8244486a7886aa56f17e3b6a3e7261
Joanna K <joannakleinrm@gmail.com>
2023-06-18 12:40:58 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,4 @@
 import propTypes from 'prop-types';
-// const { Component } = require("react");
 import css from './ContactList.module.css'
 
 export const ContactList = ({ contacts, onContactDelete }) => (
